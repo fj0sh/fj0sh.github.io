c15d0e3f167c66da25dc78db84aa211ac06cf944
fj0sh <115204470+fj0sh@users.noreply.github.com>
2024-02-27 09:52:40 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,95 +1,111 @@
 
 async function fetchMealData() {
-  const query = document.getElementById("inp")
-  try {
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + query.value);
-    let data = await response.json();
-    
-    let container = document.getElementById("resultCont");
-    container.innerHTML = "";
+    const query = document.getElementById("inp")
+    try {
+        let container = document.getElementById("resultCont");
+        container.innerHTML = "<p>Finding your recipe...</p>";
 
-    data.meals.forEach(function(recipes){
-        console.log(recipes);
-        let template = `<div class="container">
-                            <div class="header">
-                                <p>${recipes.strMeal}</p>
-                                <p>Area: ${recipes.strArea}</p>
-                                <p>Category: ${recipes.strCategory}</p>
-                            </div>
+        const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + query.value);
+        let data = await response.json();
 
-                            <div class = contents>
-                                <div class = "ingredients">`;
+        container.innerHTML = "";
 
-                                    for(let i = 0; i <= 20; i++){
-                                        const ingredients = recipes[`strIngredient${i}`];
-                                        const measurement = recipes[`strMeasure${i}`];
 
-                                        if(ingredients && measurement){
-                                            template += `<p>${measurement} ${ingredients}</p>`
-                                        }
-                                    }
-                                    
-                                template += `</div>
-                                <div class = "direction">
-                                    ${recipes.strInstructions}
+        if (!data.meals || data.meals.length === 0) {
+            let template = `<div class = "container">
+                                <p> NO RESULT FOUND</p>
+                            </div>`
+            container.insertAdjacentHTML('beforeend', template);
+        } else {
+            data.meals.forEach(function (recipes) {
+                console.log(recipes);
+                let template = `<div class="container">
+                                <div class="header">
+                                    <div class="imgCont">
+                                        <img src="${recipes.strMealThumb}">
+                                    </div>
+                                        <p><b>${recipes.strMeal}</b></p>
+                                        <p>Area: ${recipes.strArea}</p>
+                                        <p>Category: ${recipes.strCategory}</p>
+                                        <a href = ${recipes.strYoutube} target="_blank">Watch on Youtube</a>
                                 </div>
 
-                                <div class="footer"></div>
-                            </div>
-                        </div>`
+                                <div class = contents>
+                                    <div class = "ingredients">`;
 
-    container.insertAdjacentHTML('beforeend', template)
+                for (let i = 0; i <= 20; i++) {
+                    const ingredients = recipes[`strIngredient${i}`];
+                    const measurement = recipes[`strMeasure${i}`];
 
-    });
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-  }
-  
+                    if (ingredients && measurement) {
+                        template += `<p>${measurement} ${ingredients}</p>`
+                    }
+                }
+
+                template += `</div>
+                                    <div class = "direction">
+                                        ${recipes.strInstructions}
+                                    </div>
+                            </div>`
+
+                container.insertAdjacentHTML('beforeend', template)
+
+            });
+        }
+    } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+    }
 }
 
-async function generateRandom(){
-    try{
+
+async function generateRandom() {
+    try {
+        let container = document.getElementById("resultCont");
+        container.innerHTML = "<p>Finding your recipe...</p>";
+
         const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
         let data = await response.json();
-        
-        let container = document.getElementById("resultCont");
-    container.innerHTML = "";
 
-    data.meals.forEach(function(recipes){
-        console.log(recipes);
-        let template = `<div class="container">
+        container.innerHTML = "";
+
+        data.meals.forEach(function (recipes) {
+            console.log(recipes);
+            let template = `<div class="container">
+
                             <div class="header">
-                                <p>${recipes.strMeal}</p>
+                                <div class="imgCont">
+                                    <img src="${recipes.strMealThumb}">
+                                </div>
+                                <p><b>${recipes.strMeal}</b></p>
                                 <p>Area: ${recipes.strArea}</p>
                                 <p>Category: ${recipes.strCategory}</p>
+                                <a href = ${recipes.strYoutube} target="_blank">Watch on Youtube</a>
                             </div>
 
                             <div class = contents>
                                 <div class = "ingredients">`;
 
-                                    for(let i = 0; i <= 20; i++){
-                                        const ingredients = recipes[`strIngredient${i}`];
-                                        const measurement = recipes[`strMeasure${i}`];
+            for (let i = 0; i <= 20; i++) {
+                const ingredients = recipes[`strIngredient${i}`];
+                const measurement = recipes[`strMeasure${i}`];
 
-                                        if(ingredients && measurement){
-                                            template += `<p>${measurement} ${ingredients}</p>`
-                                        }
-                                    }
-                                    
-                                template += `</div>
+                if (ingredients && measurement) {
+                    template += `<p>${measurement} ${ingredients}</p>`
+                }
+            }
+
+            template += `</div>
                                 <div class = "direction">
                                     ${recipes.strInstructions}
                                 </div>
 
-                                <div class="footer"></div>
-                            </div>
                         </div>`
 
-    container.insertAdjacentHTML('beforeend', template)
+            container.insertAdjacentHTML('beforeend', template)
 
-    });
+        });
 
-    }catch (error){
+    } catch (error) {
         console.log(error);
     }
 };
